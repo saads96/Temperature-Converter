3b62a273ee37864aab326aef4639f7bee86cb366
saads96 <37954514+saads96@users.noreply.github.com>
2024-05-19 14:27:38 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const textBox = document.getElementById("textBox");
+const toFarhenheit = document.getElementById("toFarhenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result = document.getElementById("result");
+let temp;
+
+function convert() {
+    if(toFarhenheit.checked){
+    temp = Number(textBox.value);
+    temp = temp *  9/5 + 32;
+    result.textContent = temp.toFixed(1) + "°F";
+
+    }
+
+    else if(toCelsius.checked){
+        temp = Number(textBox.value);  
+        temp = (temp - 32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent = "Select a unit";
+    }
+}
